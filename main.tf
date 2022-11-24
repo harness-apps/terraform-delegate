e940d8cbec5ae461f5950817273853a49c7e64cf
Bharat Goel <bharat.goel@harness.io>
2022-11-24 22:00:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "get_harness_delegate_template" {
 	triggers = {
     delegate_name = "${var.name}"
-  }
+  	}
 	provisioner "local-exec" {
     command = "curl  https://raw.githubusercontent.com/harness-apps/delegate-template/main/harness-delegate.yaml > harness-delegate.yaml"
   }
@@ -13,66 +13,67 @@ data "local_file" "harness_delegate" {
 }
 
 resource "local_file" "delegate_name" {
-    content  = "${replace(data.local_file.harness_delegate.content,"<DELEGATE_NAME>", "${var.name}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(data.local_file.harness_delegate.content,"delegate_name", "${var.name}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "host" {
-    content  = "${replace(resource.local_file.delegate_name.content,"<DELEGATE_MANAGER_HOST>", "${var.host}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.delegate_name.content,"delegate_manager_host", "${var.host}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "accountId" {
-    content  = "${replace(resource.local_file.host.content,"<DELAGATE_ACCOUNT_ID>", "${var.accountId}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.host.content,"delegate_accountId", "${var.accountId}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "token" {
-    content  = "${replace(resource.local_file.accountId.content,"<DELEGATE_TOKEN>", "${var.token}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.accountId.content,"delegate_token", "${var.token}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "replica" {
-    content  = "${replace(resource.local_file.token.content,"<DELEGATE_REPLICA>", "${var.replica}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.token.content,"delegate_replica", "${var.replica}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "tags" {
-    content  = "${replace(resource.local_file.replica.content,"<DELEGATE_TAGS>", "${var.tags}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.replica.content,"delegate_tags", "${var.tags}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "namespace" {
-    content  = "${replace(resource.local_file.tags.content,"<DELEGATE_NAMESPACE>", "${var.namespace}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.tags.content,"delegate_ns", "${var.namespace}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "image" {
-    content  = "${replace(resource.local_file.namespace.content,"<DELEGATE_IMAGE>", "${var.image}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.namespace.content,"delegate_image", "${var.image}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "memory" {
-    content  = "${replace(resource.local_file.image.content,"<DELEGATE_MEMORY>", "${var.memory}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.image.content,"delegate_memory", "${var.memory}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "cpu" {
-    content  = "${replace(resource.local_file.memory.content,"<DELEGATE_CPU>", "${var.cpu}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.memory.content,"delegate_cpu", "${var.cpu}")}"
+    filename = "harness.yaml"
 }
 
 resource "local_file" "init_script" {
-    content  = "${replace(resource.local_file.cpu.content,"<DELEGATE_INIT_SCRIPT>", "${var.init_script}")}"
-    filename = "harness-${var.name}.yaml"
+    content  = "${replace(resource.local_file.cpu.content,"delegate_init_script", "${var.init_script}")}"
+    filename = "harness.yaml"
 }
 
 resource "null_resource" "cluster-login" {
+  
   triggers = {
     delegate_name = "${var.name}"
   }
-
   provisioner "local-exec" {
-      command = "kubectl apply -f harness-${var.name}.yaml"
+      command = "kubectl apply -f harness.yaml"
   }
+  
 }
