8b6b1bb1c0177d162bb35384bcb8fd710007a6db
Bharat Goel <bharat.goel@harness.io>
2022-11-24 10:11:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,57 +11,57 @@ data "local_file" "harness_delegate" {
 
 resource "local_file" "delegate_name" {
     content  = "${replace(data.local_file.harness_delegate.content,"<DELEGATE_NAME>", "${var.name}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "host" {
     content  = "${replace(resource.local_file.delegate_name.content,"<DELEGATE_MANAGER_HOST>", "${var.host}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "accountId" {
     content  = "${replace(resource.local_file.host.content,"<DELAGATE_ACCOUNT_ID>", "${var.accountId}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "token" {
     content  = "${replace(resource.local_file.accountId.content,"<DELEGATE_TOKEN>", "${var.token}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "replica" {
     content  = "${replace(resource.local_file.token.content,"<DELEGATE_REPLICA>", "${var.replica}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "tags" {
     content  = "${replace(resource.local_file.replica.content,"<DELEGATE_TAGS>", "${var.tags}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "namespace" {
     content  = "${replace(resource.local_file.tags.content,"<DELEGATE_NAMESPACE>", "${var.namespace}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "image" {
     content  = "${replace(resource.local_file.namespace.content,"<DELEGATE_IMAGE>", "${var.image}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "memory" {
     content  = "${replace(resource.local_file.image.content,"<DELEGATE_MEMORY>", "${var.memory}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "cpu" {
     content  = "${replace(resource.local_file.memory.content,"<DELEGATE_CPU>", "${var.cpu}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "local_file" "init_script" {
     content  = "${replace(resource.local_file.cpu.content,"<DELEGATE_INIT_SCRIPT>", "${var.init_script}")}"
-    filename = "harness.yaml"
+    filename = "harness-${var.name}.yaml"
 }
 
 resource "null_resource" "cluster-login" {
@@ -70,6 +70,9 @@ resource "null_resource" "cluster-login" {
       command = "gcloud container clusters get-credentials del-play-cluster-pr --zone us-central1-c --project del-play"
   }
   provisioner "local-exec" {
-      command = "kubectl apply -f harness.yaml"
+      command = "kubectl apply -f harness-${var.name}.yaml"
+  }
+  provisioner "local-exec" {
+      command = "rm harness-${var.name}.yaml"
   }
 }
